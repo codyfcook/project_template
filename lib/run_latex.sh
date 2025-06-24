2aea48394eea5741c1d2b5b17d35ebeb06874a7f
Cody Cook <cookcfc@gmail.com>
2025-06-24 14:04:21 +0800

    Better notes template

--- lib/run_latex.sh
+++ lib/run_latex.sh
@@ -2,27 +2,23 @@
 
 unset run_latex
 run_latex() {
-
     # Get arguments
     programname=$(basename "$1" .tex)
+    texdir=$(dirname "$1")
     outputdir="$2"
-    logfile="$3"
 
     echo "Executing: latexmk ${programname}.tex -pdf -bibtex"
-    (latexmk "${programname}.tex" -pdf -bibtex >> "${logfile}" 2>&1)
-
-    # Clean up
-    mv "${programname}.pdf" "${outputdir}"
-    rm -f "${programname}.aux"
-    rm -f "${programname}.bbl"
-    rm -f "${programname}.blg"
-    rm -f "${programname}.log" 
-    rm -f "${programname}.out"
-    rm -f "${programname}.fdb_latexmk" 
-    rm -f "${programname}.fls" 
-    rm -f "${programname}.synctex.gz"
-    rm -f "${programname}.nav" 
-    rm -f "${programname}.snm" 
-    rm -f "${programname}.toc" 
+    (
+        cd "${texdir}" || exit 1
+        latexmk "${programname}.tex" -pdf -bibtex >> "compile.log" 2>&1
+        mv "${programname}.pdf" "${outputdir}" 2>/dev/null
+        rm -f "${programname}.aux" "${programname}.bbl" "${programname}.blg" \
+              "${programname}.log" "${programname}.out" "${programname}.fdb_latexmk" \
+              "${programname}.fls" "${programname}.synctex.gz" "${programname}.nav" \
+              "${programname}.snm" "${programname}.toc"
+    )
 }
+
+# Call the function with the provided arguments
+run_latex "$1" "$2" 
 
